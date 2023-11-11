e4ad217ed0407b3f7353713bf71615d8682770b4
IvanMay <IvanMay44@yandex.ru>
2023-11-11 13:43:07 +0300

    Загрузка игры
    Изменение загрузки игры, теперь загрузка не зависит от скрипта спавна врагов

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -9,14 +9,8 @@ public class SpawnEnemy : MonoBehaviour{
 
     void Start(){
         GameScene gameScene = FindAnyObjectByType<GameScene>();
-        if (Settings.isLoadGame){
-            gameScene.enemies = new Enemy[Saver.valuesEnemies.Length];
-            for (int i = 0; i < gameScene.enemies.Length; i++){
-                gameScene.enemies[i] = Instantiate(enemy, new Vector3((float)Convert.ToDouble(Saver.valuesEnemies[i][0]), 1, (float)Convert.ToDouble(Saver.valuesEnemies[i][1])), new Quaternion());
-                gameScene.enemies[i].NewHP(Convert.ToInt32(Saver.valuesEnemies[i][2]));
-            }
+        if (Settings.isLoadGame)
             return;
-        }
         gameScene.enemies = new Enemy[countEnemy];
         for (int i = 0; i < countEnemy; i++)
             gameScene.enemies[i] = Instantiate(enemy, new Vector3(UnityEngine.Random.Range(0, 50), 1, UnityEngine.Random.Range(0, 50)), new Quaternion());
